332a16e7c8fd860572a734a1dcdb1a5c96c8814e
Carl Cassar <carlcassar@gmail.com>
2020-03-31 17:24:07 +0100

    Replace documentation app with a vanilla vue app

---
+++ src/views/Home.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="flex">
+        <div class="w-1/5">
+            kjklgjkfd
+        </div>
+        <div class="w-4/5">
+            <tw-panel>
+                Testing
+            </tw-panel>
+        </div>
+    </div>
+</template>
+
+<script>
+    // @ is an alias to /src
+    // import HelloWorld from '@/components/HelloWorld.vue'
+
+    export default {
+        name: 'Home',
+        components: {
+            // HelloWorld
+        }
+    };
+</script>
