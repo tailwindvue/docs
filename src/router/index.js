b4b029a2cde64f2fa4596ebb8ab661b927aae95b
Carl Cassar <carlcassar@gmail.com>
2020-04-01 17:08:06 +0100

    Update page title

--- src/router/index.js
+++ src/router/index.js
@@ -1,35 +1,35 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import Home from '../views/Home.vue';
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/installation',
-    name: 'Installation',
-    component: () => import(/* webpackChunkName: "installation" */ '../views/Installation')
-  },
-  {
-    path: '/headings',
-    name: 'Headings',
-    component: () => import(/* webpackChunkName: "headings" */ '../views/Headings')
-  },
-  {
-    path: '/panels',
-    name: 'Panels',
-    component: () => import(/* webpackChunkName: "panels" */ '../views/Panels')
-  },
-]
+    {
+        path: '/',
+        name: 'Home',
+        component: Home,
+    },
+    {
+        path: '/installation',
+        name: 'Installation',
+        component: () => import(/* webpackChunkName: "installation" */ '../views/Installation')
+    },
+    {
+        path: '/headings',
+        name: 'Headings',
+        component: () => import(/* webpackChunkName: "headings" */ '../views/Headings')
+    },
+    {
+        path: '/panels',
+        name: 'Panels',
+        component: () => import(/* webpackChunkName: "panels" */ '../views/Panels')
+    },
+];
 
 const router = new VueRouter({
-  mode: 'history',
-  routes
-})
+    mode: 'history',
+    routes
+});
 
-export default router
+export default router;
