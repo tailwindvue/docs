a3744c6c282b0b017534ee85046f70ded76c9e69
Carl Cassar <carlcassar@gmail.com>
2020-04-16 12:48:34 +0100

    Fix scroll to router behaviour

---
+++ app/router.scrollBehavior.js
@@ -0,0 +1,11 @@
+export default function (to, from, savedPosition) {
+    if (savedPosition) {
+        return savedPosition;
+    }
+
+    if (to.hash) {
+        return { selector: to.hash, offset: { x: 0, y: 70 } };
+    }
+
+    return { x: 0, y: 0 };
+}
