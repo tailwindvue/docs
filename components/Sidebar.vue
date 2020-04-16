b9f05ff597268981a787697b1c85a51aec375840
Carl Cassar <carlcassar@gmail.com>
2020-04-16 13:35:50 +0100

    Fix sidebar

--- components/Sidebar.vue
+++ components/Sidebar.vue
@@ -1,32 +1,60 @@
 <template>
     <div class="sticky top-24">
-        <nuxt-link class="block"
-                   to="/">Home
-        </nuxt-link>
-        <nuxt-link class="block"
-                   to="/getting-started/installation">Installation
-        </nuxt-link>
-        <nuxt-link class="block"
-                   to="/getting-started/requirements">Requirements
-        </nuxt-link>
-        <nuxt-link class="block"
-                   to="/components/alerts">Alerts
-        </nuxt-link>
-        <nuxt-link class="block"
-                   to="/components/badges">Badges
-        </nuxt-link>
-        <nuxt-link class="block"
-                   to="/components/headings">Headings
-        </nuxt-link>
-        <nuxt-link class="block"
-                   to="/components/panels">Panels
-        </nuxt-link>
-        <nuxt-link class="block"
-                   to="/components/tables">Tables
-        </nuxt-link>
-        <nuxt-link class="block"
-                   to="/components/tabs">Tabs
-        </nuxt-link>
+        <ul>
+            <li>
+                <i class="fa fa-home" />
+                <nuxt-link class=""
+                           to="/">
+                    Home
+                </nuxt-link>
+            </li>
+            <li class="uppercase text-sm text-gray-600 tracking-wide mt-4">
+                Getting Started
+            </li>
+            <li>
+                <nuxt-link class="block"
+                           to="/getting-started/installation">Installation
+                </nuxt-link>
+            </li>
+            <li>
+                <nuxt-link class="block"
+                           to="/getting-started/requirements">Requirements
+                </nuxt-link>
+            </li>
+            <li class="uppercase text-sm text-gray-600 tracking-wide mt-4">
+                Components
+            </li>
+            <li>
+                <nuxt-link class="block"
+                           to="/components/alerts">Alerts
+                </nuxt-link>
+            </li>
+            <li>
+                <nuxt-link class="block"
+                           to="/components/badges">Badges
+                </nuxt-link>
+            </li>
+            <li>
+                <nuxt-link class="block"
+                           to="/components/headings">Headings
+                </nuxt-link>
+            </li>
+            <li>
+                <nuxt-link class="block"
+                           to="/components/panels">Panels
+                </nuxt-link>
+            </li>
+            <li>
+                <nuxt-link class="block"
+                           to="/components/tables">Tables
+                </nuxt-link>
+            </li>
+            <li>
+                <nuxt-link class="block"
+                           to="/components/tabs">Tabs
+                </nuxt-link>
+            </li>
+        </ul>
     </div>
 </template>
 
