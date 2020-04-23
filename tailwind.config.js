e4528f036bd26749d0cc2f502545ca1ef18883dd
Carl Cassar <carlcassar@gmail.com>
2020-04-23 14:57:10 +0100

    Remove unused tailwind variant overrides

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,13 +9,5 @@ module.exports = {
             }
         },
     },
-    variants: {
-        cursor: ['responsive', 'hover', 'focus', 'group-hover'],
-        visibility: ['responsive', 'hover', 'focus', 'group-hover'],
-        margin: ['direct-children', 'default', 'responsive'],
-        borderWidth: ['responsive', 'first', 'hover', 'focus'],
-    },
-    plugins: [
-        require('@tailwindvue/direct-children-plugin'),
-    ]
+    variants: {},
 };
